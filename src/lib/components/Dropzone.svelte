618fbf70a7d077b4e50517c9fe8ce92d7c62a790
Marcel Gundermann <marcelgundermann@icloud.com>
2023-04-03 12:49:16 +0200

    feat: add loading indicator

--- src/lib/components/Dropzone.svelte
+++ src/lib/components/Dropzone.svelte
@@ -18,6 +18,7 @@
 	let password = '';
 	let passwordPlaceholder = '';
 	let decryptionErrorMessage = '';
+	let loading = false;
 
 	const removeAllFiles = (): void => {
 		files$.set(null);
@@ -46,6 +47,7 @@
 
 	const submitHandler = async (): Promise<void> => {
 		if (!password || !$files$) return;
+		loading = true;
 
 		const files = get(files$);
 		if (!files) return;
@@ -65,6 +67,7 @@
 			if (type === 'encrypt') {
 				const { encryptedBlob, fileName } = result as EncryptResult;
 				await downloadFile(encryptedBlob, fileName);
+				loading = false;
 			}
 		};
 
@@ -90,6 +93,9 @@
 				}
 				const { decryptedBlob, fileName } = decryptedFile;
 				await downloadFile(decryptedBlob, fileName);
+
+				decryptionErrorMessage = '';
+				loading = false;
 			}
 		};
 
@@ -379,22 +385,27 @@
 	{/if}
 
 	{#if $files$ && password}
-		{#if mode === 'encrypt'}
-			<button
-				on:click={submitHandler}
-				type="button"
-				class="mt-2 w-full rounded-lg bg-white px-3.5 py-2.5 text-sm text-black hover:bg-white/90 transition-all"
-			>
-				Encrypted Files
-			</button>
-		{:else if mode === 'decrypt'}
-			<button
-				on:click={submitHandler}
-				type="button"
-				class="mt-2 w-full rounded-lg bg-white px-3.5 py-2.5 text-sm text-black hover:bg-white/90 transition-all"
-			>
-				Decrypted Files
-			</button>
-		{/if}
+		<button
+			on:click={submitHandler}
+			type="button"
+			class="mt-2 inline-flex items-center justify-center w-full rounded-lg bg-white px-3.5 py-2.5 text-sm text-black hover:bg-white/90 transition-all"
+		>
+			{#if loading}
+				<svg
+					class="animate-spin -ml-1 mr-2 h-4 w-4 text-black"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+					<path
+						class="opacity-75"
+						fill="currentColor"
+						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+					/>
+				</svg>
+			{/if}
+			{mode === 'encrypt' ? 'Encrypt Files' : 'Decrypt Files'}
+		</button>
 	{/if}
 </form>
