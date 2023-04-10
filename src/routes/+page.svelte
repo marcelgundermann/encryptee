11b7512bb4a1d3421e181368ea063a037ab7b9d7
Marcel Gundermann <marcelgundermann@icloud.com>
2023-04-10 13:11:06 +0200

    refac: add DropzoneOverlay to root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Dropzone from '$lib/components/Dropzone.svelte';
+	import DropzoneOverlay from '$lib/components/DropzoneOverlay.svelte';
 </script>
 
 <svelte:head>
@@ -10,6 +11,11 @@
 	/>
 </svelte:head>
 
-<div class="w-full h-full flex flex-col items-center pt-28">
+<div class="max-w-screen-sm mx-auto w-full h-full flex flex-col items-center pt-28">
+	<h1 class="pb-10 md:text-lg text-white/70 font-cal tracking-wide text-center">
+		Encrypt and Decrypt your files in the browser fast and secure
+	</h1>
 	<Dropzone />
 </div>
+
+<DropzoneOverlay />
