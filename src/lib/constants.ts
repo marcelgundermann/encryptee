da10f93f7880917a04e98079973fddf85965fb56
Marcel Gundermann <marcelgundermann@icloud.com>
2023-04-10 13:06:26 +0200

    feat: add functions for reading file in chunks

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const CHUNK_SIZE = 50 * 1_024 * 1_024;
+
+export const TAG_LENGTH = 16;
+export const IV_BYTE_LENGTH = 12;
+export const SALT_BYTE_LENGTH = 16;
--- src/lib/store/files.ts
+++ src/lib/store/files.ts
@@ -1,7 +1,15 @@
 // store.ts
-import { writable } from 'svelte/store';
+import type { CipherOperationChunkState, CipherOperationFileState, Mode } from '$lib/types';
+import { derived, writable } from 'svelte/store';
 
 export const files$ = writable<FileList | null>();
+export const abortedFiles$ = writable<Set<string>>();
+
+export const isFastModeSupported$ = writable<boolean>(true);
+export const isFastMode$ = writable<boolean>(false);
+export const password$ = writable<string>('');
+export const applicationMode$ = writable<Mode>('encrypt');
+export const cipherOperationState$ = writable<CipherOperationFileState | CipherOperationChunkState | null>(null);
 
 export const addFiles = (newFiles: FileList) => {
 	files$.update((store) => {
@@ -40,3 +48,50 @@ export const removeFile = (fileToRemove: File) => {
 		return fileList.files;
 	});
 };
+
+export const addAbortedFiles = (abortedFiles: Array<string>) => {
+	abortedFiles$.update((store) => {
+		if (!store) return new Set(abortedFiles);
+
+		for (const file of abortedFiles) {
+			store.add(file);
+		}
+
+		return store;
+	});
+};
+
+export const addAbortedFile = (file: File) => {
+	abortedFiles$.update((store) => {
+		if (!store) return new Set([file.name]);
+
+		store.add(file.name);
+
+		return store;
+	});
+};
+
+/**
+ * Determines the mode (encrypt or decrypt) based on the file extensions in the given FileList.
+ * If all files have the '.cre' extension, the mode is set to 'decrypt'.
+ * If there are mixed extensions or none of them is '.cre', the mode is set to 'encrypt'.
+ * If no files are provided, the default mode is 'encrypt'.
+ *
+ * @param {FileList | null} files - The list of files to determine the mode from.
+ *
+ * @returns {Mode} - The determined mode ('encrypt' or 'decrypt').
+ */
+export const cryptoMode$ = derived(files$, (files): Mode => {
+	if (!files) return 'encrypt';
+
+	const fileExtensionSet = new Set<string>();
+
+	for (const { name } of files) {
+		fileExtensionSet.add(name.substring(name.lastIndexOf('.')).toLowerCase());
+	}
+
+	if (fileExtensionSet.size === 1 && fileExtensionSet.has('.cre')) return 'decrypt';
+	if (fileExtensionSet.size > 1 && fileExtensionSet.has('.cre')) return 'mixed';
+
+	return 'encrypt';
+});
