e307b7b1dcbaa746b375367042dfa18fea5b4f24
Marcel Gundermann <marcelgundermann@icloud.com>
2023-06-18 11:35:39 +0200

    chore+style: cleanup layout, bg color updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import 'cal-sans';
-
-	import { onMount } from 'svelte';
-	import { isFastModeSupported$ } from '$lib/store/files';
 	import Header from '$lib/components/Header.svelte';
-
-	onMount(() => {
-		isFastModeSupported$.set('showDirectoryPicker' in window);
-	});
 </script>
 
 <main class="max-w-screen-xl mx-auto p-5 sm:p-10 h-screen">
